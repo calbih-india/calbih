b0f689e1a865ae8491ceffdab10aa473c407f04d
shakir <shakirl88@gmail.com>
2019-08-16 12:38:37 +0530

    ok

--- calbihindia/settings.py
+++ calbihindia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&k$!+0#2fb5zjws^tigi5dgrl*0d1suo*ruj2f+xjhu*lb0k7q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://165.22.219.107','localhost','http://127.0.0.1']
 
 
 # Application definition
